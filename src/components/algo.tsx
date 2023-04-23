533c0f0f1818e2b03bd640cf43a10896e03848b4
Matt Kopra <matt.kopra@gmail.com>
2023-04-22 23:19:16 -0500

    cleaned up algos

--- src/components/algo.tsx
+++ src/components/algo.tsx
@@ -3,74 +3,30 @@ function algo(onerepmax: number, tworepmax: number, threerepmax: number, fourrep
     let RMs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     RMs[0] = onerepmax
-    RMs[1] = RMs[0] * multip[1]
-    RMs[2] = RMs[0] * multip[2]
-    RMs[3] = RMs[0] * multip[3]
-    RMs[4] = RMs[0] * multip[4]
-    RMs[5] = RMs[0] * multip[5]
-    RMs[6] = RMs[0] * multip[6]
-    RMs[7] = RMs[0] * multip[7]
-    RMs[8] = RMs[0] * multip[8]
-    RMs[9] = RMs[0] * multip[9]
-    RMs[10] = RMs[0] * multip[10]
-    RMs[11] = RMs[0] * multip[11]
+    for (let i = 0; i < multip.length; i++)
+            RMs[i] = (((RMs[0]) * multip[i]))
+    
 
 
     if (onerepmax == 0 && tworepmax != 0) {
         RMs[0] = tworepmax / multip[1]
-        RMs[1] = RMs[0] * multip[1]
-        RMs[2] = RMs[0] * multip[2]
-        RMs[3] = RMs[0] * multip[3]
-        RMs[4] = RMs[0] * multip[4]
-        RMs[5] = RMs[0] * multip[5]
-        RMs[6] = RMs[0] * multip[6]
-        RMs[7] = RMs[0] * multip[7]
-        RMs[8] = RMs[0] * multip[8]
-        RMs[9] = RMs[0] * multip[9]
-        RMs[10] = RMs[0] * multip[10]
-        RMs[11] = RMs[0] * multip[11]
+        for (let i = 0; i < multip.length; i++)
+            RMs[i] = (((RMs[0]) * multip[i]))
     }
     if (onerepmax == 0 && tworepmax == 0 && threerepmax != 0) {
         RMs[0] = threerepmax / multip[2]
-        RMs[1] = RMs[0] * multip[1]
-        RMs[2] = RMs[0] * multip[2]
-        RMs[3] = RMs[0] * multip[3]
-        RMs[4] = RMs[0] * multip[4]
-        RMs[5] = RMs[0] * multip[5]
-        RMs[6] = RMs[0] * multip[6]
-        RMs[7] = RMs[0] * multip[7]
-        RMs[8] = RMs[0] * multip[8]
-        RMs[9] = RMs[0] * multip[9]
-        RMs[10] = RMs[0] * multip[10]
-        RMs[11] = RMs[0] * multip[11]
+        for (let i = 0; i < multip.length; i++)
+            RMs[i] = (((RMs[0]) * multip[i]))
     }
     if (onerepmax == 0 && tworepmax == 0 && threerepmax == 0 && fourrepmax != 0) {
         RMs[0] = fourrepmax / multip[3]
-        RMs[1] = RMs[0] * multip[1]
-        RMs[2] = RMs[0] * multip[2]
-        RMs[3] = RMs[0] * multip[3]
-        RMs[4] = RMs[0] * multip[4]
-        RMs[5] = RMs[0] * multip[5]
-        RMs[6] = RMs[0] * multip[6]
-        RMs[7] = RMs[0] * multip[7]
-        RMs[8] = RMs[0] * multip[8]
-        RMs[9] = RMs[0] * multip[9]
-        RMs[10] = RMs[0] * multip[10]
-        RMs[11] = RMs[0] * multip[11]
+        for (let i = 0; i < multip.length; i++)
+            RMs[i] = (((RMs[0]) * multip[i]))
     }
     if (onerepmax == 0 && tworepmax == 0 && threerepmax == 0 && fourrepmax == 0 && fiverepmax != 0) {
         RMs[0] = fiverepmax / multip[4]
-        RMs[1] = RMs[0] * multip[1]
-        RMs[2] = RMs[0] * multip[2]
-        RMs[3] = RMs[0] * multip[3]
-        RMs[4] = RMs[0] * multip[4]
-        RMs[5] = RMs[0] * multip[5]
-        RMs[6] = RMs[0] * multip[6]
-        RMs[7] = RMs[0] * multip[7]
-        RMs[8] = RMs[0] * multip[8]
-        RMs[9] = RMs[0] * multip[9]
-        RMs[10] = RMs[0] * multip[10]
-        RMs[11] = RMs[0] * multip[11]
+        for (let i = 0; i < multip.length; i++)
+            RMs[i] = (((RMs[0]) * multip[i]))
     }
     if (tworepmax != 0)
         RMs[1] = tworepmax
@@ -80,18 +36,9 @@ function algo(onerepmax: number, tworepmax: number, threerepmax: number, fourrep
         RMs[3] = fourrepmax
     if (fiverepmax != 0)
         RMs[4] = fiverepmax
-    RMs[0] = parseFloat(RMs[0].toFixed(1))
-    RMs[1] = parseFloat(RMs[1].toFixed(1))
-    RMs[2] = parseFloat(RMs[2].toFixed(1))
-    RMs[3] = parseFloat(RMs[3].toFixed(1))
-    RMs[4] = parseFloat(RMs[4].toFixed(1))
-    RMs[5] = parseFloat(RMs[5].toFixed(1))
-    RMs[6] = parseFloat(RMs[6].toFixed(1))
-    RMs[7] = parseFloat(RMs[7].toFixed(1))
-    RMs[8] = parseFloat(RMs[8].toFixed(1))
-    RMs[9] = parseFloat(RMs[9].toFixed(1))
-    RMs[10] = parseFloat(RMs[10].toFixed(1))
-    RMs[11] = parseFloat(RMs[11].toFixed(1))
+    
+    for (let i = 0; i < RMs.length; i++)
+        RMs[i] = parseFloat(RMs[i].toFixed(1))
     
     return RMs
 }
