1e4b8025fd0d3e2b8b319cec04730309f0615286
Matt Kopra <matt.kopra@gmail.com>
2023-07-02 16:40:03 -0500

    utils User import

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import { User } from './login';
 
-export var USER: any = "6498904c419d284a1cff301a"
+
+//export var USER: any = "6498904c419d284a1cff301a"
 //export var USER: undefined
 
 const BASE_URL = 'http://127.0.0.1:8000';
@@ -9,7 +11,11 @@ export async function fetchData(userId: string): Promise<any> {
   const url = `${BASE_URL}/exercises/${userId}`;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url,  {
+      headers: {
+        Authorization: `Bearer ${User}`,
+      },
+    }); // chnage made
     return response.data;
   } catch (error) {
     throw error;
